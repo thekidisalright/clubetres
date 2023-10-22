784fe7722e1e260a56b7968550a9d462b559e02c
thekidisalright <dev.alexandresilva@gmail.com>
2023-10-22 19:47:51 -0300

    banner done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,50 +1,66 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="author" content="Alexandre Silva">
-        <meta name="description" content="Fork do site Clube TRES para a disciplina de Programação Web II, do professor Renan Praxedes">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Alexandre Silva">
+    <meta name="description"
+        content="Fork do site Clube TRES para a disciplina de Programação Web II, do professor Renan Praxedes">
 
-        <title>Clube TRES</title>
+    <title>Clube TRES</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Jost:wght@300;400;500;700&family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Jost:wght@300;400;500;700&family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap"
+        rel="stylesheet">
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-        <link rel="stylesheet" href="css/style.css">
-        <link rel="shortcut icon" href="images/clube_tres_video_branco.png" type="image/x-icon">
-    </head>
-    <body>
+    <!-- Styles -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href="images/clube_tres_video_branco.png" type="image/x-icon">
+</head>
+<body>
 
-        <div class="container-fluid">
-            <div class="row navbar">
-                <div class="row">
-                    <div class="col logo-navbar py-2">
-                        <img src="images/home/logotres.png" alt="logo TRES">
-                    </div>
-                    <div class="col-auto py-4">
-                        <div class="col">
-                            <a class="nav-item" id="sobre">SOBRE A TRES®</a>
-                            <a class="nav-item" id="sac">SAC</a>
-                            <button type="button" class="btn btn-comprar nav-item ms-5">COMPRAR</button>
-                        </div>
+    <div class="container-fluid">
+        <div class="row navbar">
+            <div class="row">
+                <div class="col logo-navbar py-2">
+                    <img src="images/home/logotres.png" alt="logo TRES">
+                </div>
+                <div class="col-auto py-4">
+                    <div class="col">
+                        <a class="nav-item" id="sobre">SOBRE A TRES®</a>
+                        <a class="nav-item" id="sac">SAC</a>
+                        <button type="button" class="btn btn-comprar nav-item ms-5">COMPRAR</button>
                     </div>
                 </div>
-                <div class="row justify-content-center mb-2">
-                    <div class="col-md-10 col-lg-8 d-flex justify-content-between">
-                        <div class="col-auto"><a href="" class="ico-navbar"><img src="images/home/ico_login.png" alt="Login" style="width: 49px; height: 34px;"><span>LOGIN</span></a></div>
-                        <div class="col-auto"><a href="" class="ico-navbar"><img src="images/home/ico_participar.png" alt="Participar" style="width: 32px; height: 38px;"><span>BENEFÍCIOS</span></a></div>
-                        <div class="col-auto"><a href="" class="ico-navbar"><img src="images/home/ico_cadastro.png" alt="Cadastro" style="width: 33px; height: 38px;"><span>CADASTRE-SE</span></a></div>
-                        <div class="col-auto"><a href="" class="ico-navbar"><img src="images/home/ico_regulamento.png" alt="Regulamento" style="width: 33px; height: 38px;"><span>REGULAMENTO</span></a></div>
-                        <div class="col-auto"><a href="" class="ico-navbar"><img src="images/home/ico_reciclagem.png" alt="Reciclagem" style="width: 32px; height: 38px;"><span>RECICLAGEM</span></a></div>
-                    </div>
+            </div>
+            <div class="row justify-content-center mb-2">
+                <div class="col-md-10 col-lg-8 d-flex justify-content-between">
+                    <div class="col-auto"><a href="" class="ico-navbar"><img src="images/home/ico_login.png" alt="Login" style="width: 49px; height: 34px;"><span>LOGIN</span></a></div>
+                    <div class="col-auto"><a href="" class="ico-navbar"><img src="images/home/ico_participar.png" alt="Participar" style="width: 32px; height: 38px;"><span>BENEFÍCIOS</span></a></div>
+                    <div class="col-auto"><a href="" class="ico-navbar"><img src="images/home/ico_cadastro.png" alt="Cadastro" style="width: 33px; height: 38px;"><span>CADASTRE-SE</span></a></div>
+                    <div class="col-auto"><a href="" class="ico-navbar"><img src="images/home/ico_regulamento.png" alt="Regulamento" style="width: 33px; height: 38px;"><span>REGULAMENTO</span></a></div>
+                    <div class="col-auto"><a href="" class="ico-navbar"><img src="images/home/ico_reciclagem.png" alt="Reciclagem" style="width: 32px; height: 38px;"><span>RECICLAGEM</span></a></div>
                 </div>
             </div>
         </div>
 
-    </body>
+        <div class="row">
+            <div class="col-5 banner" style="height: 500px">
+                <img src="images/clube_tres_video_branco.png" alt="Clube TRES" class="img-banner mt-5 col-md-4 col-6">
+                <h2 class="fs-5 mt-3">Programa de benefícios para quem é apaixonado por sabores.</h2>
+                <p>Novidades em primeira mão, descontos em produtos, conteúdo exclusivo e muito mais.</p>
+                <a href="">CLIQUE E PARTICIPE!</a>
+            </div>
+            <div class="col-7 p-0" style="height: 500px; position: relative;">
+                <img src="images/banner_home.jpg" class="h-100 w-100 object-fit-cover" alt="Clube TRES Evoluiu" style="object-position: right;">
+            </div>
+        </div>
+
+
+    </div>
+</body>
 </html>
