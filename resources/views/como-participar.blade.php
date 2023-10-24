e037d6563540da6c6f6fe9e3ae3e7039f6c0cc2d
thekidisalright <dev.alexandresilva@gmail.com>
2023-10-24 11:27:34 -0300

    rows principais do como-participar

---
+++ resources/views/como-participar.blade.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Alexandre Silva">
+    <meta name="description"
+        content="Fork do site Clube TRES para a disciplina de Programação Web II, do professor Renan Praxedes">
+
+    <title>Clube TRES</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Jost:wght@300;400;500;700&family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap"
+        rel="stylesheet">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href="images/clube_tres_video_branco.png" type="image/x-icon">
+</head>
+<body class="como-participar">
+    <div class="container-fluid">
+
+        <div class="row navbar">
+            <div class="row">
+                <div class="col logo-navbar py-2">
+                    <img src="images/home/logotres.png" alt="logo TRES">
+                </div>
+                <div class="col-auto py-4">
+                    <div class="col">
+                        <a class="nav-item" id="sobre">SOBRE A TRES®</a>
+                        <a class="nav-item" id="sac">SAC</a>
+                        <button type="button" class="btn btn-comprar nav-item ms-5">COMPRAR</button>
+                    </div>
+                </div>
+            </div>
+            <div class="row justify-content-center mb-2">
+                <div class="col-md-10 col-lg-8 d-flex justify-content-between align-items-center">
+                    <div class="col-auto"><a href="" class="ico-navbar"><img src="images/home/ico_login.png" alt="Login" style="width: 49px; height: 34px;"><span>LOGIN</span></a></div>
+                    <div class="col-auto"><a href="" class="ico-navbar"><img src="images/home/ico_participar.png" alt="Participar" style="width: 32px; height: 38px;"><span>BENEFÍCIOS</span></a></div>
+                    <div class="col-auto"><a href="" class="ico-navbar"><img src="images/home/ico_cadastro.png" alt="Cadastro" style="width: 33px; height: 38px;"><span>CADASTRE-SE</span></a></div>
+                    <div class="col-auto"><a href="" class="ico-navbar"><img src="images/home/ico_regulamento.png" alt="Regulamento" style="width: 33px; height: 38px;"><span>REGULAMENTO</span></a></div>
+                    <div class="col-auto"><a href="" class="ico-navbar"><img src="images/home/ico_reciclagem.png" alt="Reciclagem" style="width: 32px; height: 38px;"><span>RECICLAGEM</span></a></div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row mb-5">
+            <div class="col-5 banner" style="height: 500px">
+                <img src="images/clube_tres_video_branco.png" alt="Clube TRES" class="img-banner mt-5 col-md-4 col-6">
+                <h2 class="fs-5 mt-3 overflow-hiddden">Como participar</h2>
+                <p>CADASTRE OS CÓDIGOS DAS CAIXAS DE CÁPSULAS PARA ATINGIR UMA CATEGORIA DE BENEFÍCIOS E GANHAR VANTAGENS NO E-COMMERCE TRES®.</p>
+            </div>
+            <div class="col-7 p-0" style="height: 500px; position: relative;">
+                <img src="images/participar/foto_topo.jpg" class="h-100 w-100 object-fit-cover" alt="Clube TRES Evoluiu" style="object-position: right;">
+            </div>
+        </div>
+
+        <div class="row txt-5">
+            <h3>Ao se cadastrar no Clube TRES®</h3>
+            <h3>você já é um <span>EXPLORADOR</span> e ganha descontos.</h3>
+        </div>
+
+        <div class="row txt-6 mt-5 mb-5">
+            <h3>São 3 (tres) categorias de benefícios que você pode atingir:<br><span>Explorador, Apaixonado e Entusiasta</span></h3>
+        </div>
+
+        <div class="row txt-7 mb-5">
+            <h3>
+                Além disso, compras realizadas no e-commerce <strong>TRES®</strong><br><span>Valem o dobro de códigos</span> promocionais<br>cadastrados no <strong>clube tres®.</strong>
+                <br><br>
+                Aniversariantes têm direito a experiências especiais<br> e <strong>ofertas exclusivas</strong>
+            </h3>
+        </div>
+
+        <div class="row">
+            <div class="col tipo-cliente p-5" id="explorador">
+                <div class="col-12 d-flex justify-content-end"><img src="images/ico_explorador.png" alt="icone" class="img-fluid" style="max-width: 90px;"></div>
+                <h3>Seja um<br><strong>Explorador</strong></h3>
+                <p class="tc-txt-1">Esse é o primeiro nível ao se cadastrar no Clube TRES®.</p>
+                <p class="tc-txt-2">Aproveite descontos no <strong>e-commerce tres®</strong>:</p>
+                <p class="tc-txt-3"><strong>- 29% na sua cápsula favorita e 27% </strong>nas demais cápsulas.</p>
+                <p class="tc-txt-3"><strong>- 5%</strong> em máquinas e acessórios.</p>
+                <p class="tc-txt-3">Fique sabendo dos <span>lançamentos da TRES®</span> em primeira mão, <span>receba promoções de cápsulas, descontos exclusivos</span> e muito mais.</p>
+                <small>*Dentro de um período de 12 meses.</small>
+
+            </div>
+            <div class="col tipo-cliente p-5" id="apaixonado">
+                <div class="col-12 d-flex justify-content-end"><img src="images/ico_apaixonado.png" alt="icone" class="img-fluid" style="max-width: 90px;"></div>
+                <h3>Vire um<br><strong>Apaixonado</strong></h3>
+                <p class="tc-txt-1">Cadastre no mínimo <strong>26 caixas</strong> de cápsulas*.</p>
+                <p class="tc-txt-2">Aproveite descontos no <strong>e-commerce tres®</strong>:</p>
+                <p class="tc-txt-3"><strong>- 29%</strong> em qualquer cápsula.</p>
+                <p class="tc-txt-3"><strong>- 10%</strong> em máquinas e acessórios.</p>
+                <p class="tc-txt-3">Fique sabendo dos <span>lançamentos da TRES®</span> em primeira mão, <span>receba promoções de cápsulas, descontos exclusivos</span> e muito mais. E no <span>mês do seu aniversário</span> comemore com uma <span>promoção especial e imperdível.</span></p>
+                <small>*Dentro de um período de 12 meses.</small>
+            </div>
+            <div class="col tipo-cliente p-5" id="entusiasta">
+                <div class="col-12 d-flex justify-content-end"><img src="images/ico_entusiasta.png" alt="icone" class="img-fluid" style="max-width: 90px;"></div>
+                <h3>Se torne um<br><strong>Entusiasta</strong></h3>
+                <p class="tc-txt-1">Cadastre a partir de <strong>41 caixas</strong> de cápsulas*.</p>
+                <p class="tc-txt-2">Aproveite descontos no <strong>e-commerce tres®</strong>:</p>
+                <p class="tc-txt-3">- 31% <strong>em qualquer cápsula.</strong></p>
+                <p class="tc-txt-3">- 15% <strong>em máquinas e</strong> 10% <strong>em acessórios.</strong></p>
+                <p class="tc-txt-3">Fique sabendo dos <span>lançamentos da TRES®</span> em primeira mão, <span>receba promoções de cápsulas, descontos exclusivos</span> e muito mais. E no <span>mês do seu aniversário</span> comemore com uma <span>promoção especial e imperdível.</span></p>
+                <small>*Dentro de um período de 12 meses.</small>
+            </div>
+        </div>
+
+        <div class="row"></div>
+    
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/como-participar", function() {
+    return view("como-participar");
+});
